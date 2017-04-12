314ed697d71d5890f6a44ee8da47ecb9dc83c004
sicos1977 <sicos2002@hotmail.com>
2017-04-12 18:56:12 +0200

    Added example program

---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using PageOrientationEngine;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var poe = new DocumentInspector(@"c:\Program Files (x86)\Tesseract-OCR\tessdata\", "eng");
+            var results = poe.DetectPageOrientation(@"<some single or multipage tiff file");
+            foreach(var result in results)
+                Console.WriteLine(result.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
